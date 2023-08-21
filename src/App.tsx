42973f922db1673c648969b486704399769ce75b
vineet2020 <vineetgoel0517@gmail.com>
2023-08-21 14:52:52 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import AddContactForm from './components/AddContactForm';
-// import ContactList from './components/ContactList';
-// import ContactDetails from './components/ContactDetails';
 import ContactPage from './pages/ContactPage';
 import './styles/styles.css';
+import './styles/leaflet.css';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-        {/* <Route path='/' element={<ContactList/>} /> */}
-        {/* <Route path='/addcontact' element={<AddContactForm/>} /> */}
-        {/* <Route path='/details/:id' element={<ContactDetails/>} /> */}
         <Route path='/' element={<ContactPage/>} />
         </Routes>
       </div>
