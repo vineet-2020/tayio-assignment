eb7b397b4b9eda0c2b1cf6ec47aa00d9f56f9fda
vineet2020 <vineetgoel0517@gmail.com>
2023-08-21 15:20:40 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import './styles/styles.css';
-import './styles/leaflet.css';
 
 const App: React.FC = () => {
   return (
